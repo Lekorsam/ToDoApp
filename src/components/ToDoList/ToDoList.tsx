464f1a0619d2f956e4c6da502ef81098b8231c0f
Elena <elena.samokhvalova@procaltech.com>
2025-03-07 14:26:43 +0300

    local storage - save correct filter when update page

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TasksFilter, Todo } from "../../types/types";
 import styles from "./ToDoList.module.scss";
 import TodoItem from "../ToDoItem";
@@ -8,8 +8,17 @@ type TodoListProps = {
   onToggleChange: (id: number) => void;
 };
 
+const STORAGE_KEY = "activeFilter";
+
 const TodoList: React.FC<TodoListProps> = ({ tasks, onToggleChange }) => {
-  const [filter, setFilter] = useState<TasksFilter>(TasksFilter.All);
+  const [filter, setFilter] = useState<TasksFilter>(() => {
+    const savedFilter = localStorage.getItem(STORAGE_KEY);
+    return savedFilter ? JSON.parse(savedFilter) : TasksFilter.All;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(filter));
+  }, [filter]);
 
   const filteredTodos =
     filter === TasksFilter.All
